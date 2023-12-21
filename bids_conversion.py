1e46f3d896d1eef6c80b0a81b783132905e4ba15
jcohenadad <jcohen@polymtl.ca>
2023-12-21 16:14:52 -0500

    Fixed location of GRE scans-- should go under anat

--- bids_conversion.py
+++ bids_conversion.py
@@ -24,12 +24,12 @@ for shimtype in ['noRFshim', 'CVred']:
         file = [os.path.join(path_in, 'MPRAGE', f) for f in os.listdir(os.path.join(path_in, 'MPRAGE')) if shimtype in f and f.endswith(ext)][0]
         shutil.copy2(file, os.path.join(path_out, f'anat/sub-{subject}_acq-{shimtype_dict[shimtype]}_T1w.{ext}'))
 
-# Convert GRE data
+# Convert files in GRE_B1 subfolder
 # Get the absolute file name of the NIfTI and JSON files under each subfolder of the GRE_B1 subfolder that has the string corresponding to the name of the subfolder in it, and that has the string "gre2d" in it. Select the first pair in the list.
 for shimtype in ['CVred', 'Noshim']:
     for ext in ['nii.gz', 'json']:
+        # Convert GRE data
         file = [os.path.join(path_in, 'GRE_B1', shimtype, f) for f in os.listdir(os.path.join(path_in, 'GRE_B1', shimtype)) if 'gre2d' in f and f.endswith(ext)][0]
-        shutil.copy2(file, os.path.join(path_out, f'fmap/sub-{subject}_acq-{shimtype_dict[shimtype]}_T2starw.{ext}'))
+        shutil.copy2(file, os.path.join(path_out, f'anat/sub-{subject}_acq-{shimtype_dict[shimtype]}_T2starw.{ext}'))
+        # Convert RF map data
 
-
-        
