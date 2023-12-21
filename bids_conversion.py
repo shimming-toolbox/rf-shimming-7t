270f3072c6ffc52223c0d7a89225abcef8ccb2a5
jcohenadad <jcohen@polymtl.ca>
2023-12-21 16:12:59 -0500

    Working GRE conversion

--- bids_conversion.py
+++ bids_conversion.py
@@ -5,7 +5,9 @@ path_in = '/Users/julien/code/rf-shimming-7t/RF_shimming_project_cleanupload/Sub
 path_out = '/Users/julien/Desktop/rf_shimming_spinalcord/sub-02'
 
 # Create dictionary for shim type output
-shimtype_dict = {'noRFshim': 'CP', 'CVred': 'CVred'}
+shimtype_dict = {'noRFshim': 'CP',
+                 'Noshim': 'CP',
+                 'CVred': 'CVred'}
 
 # Get subject name from path_out
 subject = os.path.basename(path_out)
@@ -19,10 +21,15 @@ os.makedirs(os.path.join(path_out, 'fmap'), exist_ok=True)
 # Get the absolute file name of the NIfTI and JSON files under the MPRAGE subfolder, that has the string "noRFshim" in it
 for shimtype in ['noRFshim', 'CVred']:
     for ext in ['nii.gz', 'json']:
-        mprage_file = [os.path.join(path_in, 'MPRAGE', f) for f in os.listdir(os.path.join(path_in, 'MPRAGE')) if shimtype in f and f.endswith(ext)][0]
-        shutil.copy2(mprage_file, os.path.join(path_out, f'anat/sub-{subject}_acq-{shimtype_dict[shimtype]}_T1w.{ext}'))
+        file = [os.path.join(path_in, 'MPRAGE', f) for f in os.listdir(os.path.join(path_in, 'MPRAGE')) if shimtype in f and f.endswith(ext)][0]
+        shutil.copy2(file, os.path.join(path_out, f'anat/sub-{subject}_acq-{shimtype_dict[shimtype]}_T1w.{ext}'))
 
-# Convert RF map data
+# Convert GRE data
+# Get the absolute file name of the NIfTI and JSON files under each subfolder of the GRE_B1 subfolder that has the string corresponding to the name of the subfolder in it, and that has the string "gre2d" in it. Select the first pair in the list.
+for shimtype in ['CVred', 'Noshim']:
+    for ext in ['nii.gz', 'json']:
+        file = [os.path.join(path_in, 'GRE_B1', shimtype, f) for f in os.listdir(os.path.join(path_in, 'GRE_B1', shimtype)) if 'gre2d' in f and f.endswith(ext)][0]
+        shutil.copy2(file, os.path.join(path_out, f'fmap/sub-{subject}_acq-{shimtype_dict[shimtype]}_T2starw.{ext}'))
 
 
         
